e39a3fd554753e4bc2ed8466029f8b43a75019d4
Antonio Sitoe <ubivoidmoz@gmail.com>
2024-01-05 16:44:17 +0200

    estrutura da pagina inicial feita

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,139 @@
-import { Button, StyleSheet } from 'react-native'
+import { Button, StyleSheet, TouchableOpacity } from 'react-native'
 import { Text, View } from '@/components/Themed'
 import { HeaderModular } from '@/components/ui/HeaderModular'
 import { useRouter } from 'expo-router'
+import { ScrollView } from 'react-native-gesture-handler'
+import { fonts } from '@/constants/fonts'
+
+import HandSwatch from '@/assets/Icons/HandSwatch.svg'
+import Colors from '@/constants/Colors'
+import React from 'react'
 
 export default function Home() {
-	const router = useRouter()
+	const { push } = useRouter()
 	return (
 		<>
 			<HeaderModular isDefault />
 			<View style={styles.container}>
 				<View style={[styles.containerChild, styles.containerChildWithMB]}>
 					<View style={styles.child}>
-						<Text>Informação</Text>
-						<Button
-							title="gogt"
-							onPress={() => {
-								router.push('/(entertainments)/games')
-							}}
-						/>
+						<Text style={styles.textTitle}>Informação</Text>
+						<View style={[styles.mainCards]}>
+							<TouchableOpacity style={styles.card}></TouchableOpacity>
+							<TouchableOpacity style={styles.card}></TouchableOpacity>
+							<TouchableOpacity style={styles.card}></TouchableOpacity>
+							<TouchableOpacity style={styles.card}></TouchableOpacity>
+						</View>
 					</View>
-					<View style={styles.child2}>
-						<Text>Metereologia</Text>
+					<View
+						style={[
+							styles.child2,
+							{
+								flexDirection: 'row',
+								gap: 23,
+							},
+						]}
+					>
+						<View style={{ flex: 1 }}>
+							<Text style={styles.textTitle}>Metereologia</Text>
+							<View style={styles.cardMeteorlogia}></View>
+						</View>
+						<View>
+							<Text style={styles.textTitle}>Powered By</Text>
+							<View style={styles.mainCardWhiteSmokeContainer}>
+								<TouchableOpacity
+									style={styles.cardWhitesmoke}
+								></TouchableOpacity>
+								<TouchableOpacity
+									style={styles.cardWhitesmoke}
+								></TouchableOpacity>
+							</View>
+						</View>
 					</View>
 				</View>
-				{/* <View style={styles.containerChild}></View> */}
+				<View style={[styles.containerChild, styles.containerChildWithMB]}>
+					<View style={styles.child}>
+						<Text style={styles.textTitle}>Entretenimento</Text>
+						<View
+							style={[
+								styles.mainCards,
+								{
+									backgroundColor: Colors.light.smokeWhite,
+								},
+							]}
+						>
+							{[1, 2, 3, 4].map((item) => {
+								return (
+									<TouchableOpacity
+										onPress={() => push('/(entertainments)/games')}
+										key={item}
+										style={[
+											styles.card,
+											{
+												backgroundColor: Colors.light.lavenderBlush,
+											},
+										]}
+									></TouchableOpacity>
+								)
+							})}
+						</View>
+					</View>
+					<View style={styles.child2}>
+						<Text
+							style={[
+								styles.textTitle,
+								{
+									color: Colors.light.charcoal,
+								},
+							]}
+						>
+							Outros |{' '}
+							<Text
+								style={{
+									fontSize: 14,
+									color: Colors.light.charcoal,
+									flexDirection: 'row',
+									alignItems: 'center',
+									gap: 6,
+									fontFamily: fonts.fontFamyle.Gilroy_extraBold,
+								}}
+							>
+								Arrasta <HandSwatch />
+							</Text>
+						</Text>
+						<View
+							style={{
+								padding: 10,
+								backgroundColor: Colors.light.smokeWhite,
+								borderRadius: 9,
+							}}
+						>
+							<ScrollView
+								horizontal
+								showsHorizontalScrollIndicator={false}
+								contentContainerStyle={{
+									gap: 23,
+									height: 'auto',
+									flexDirection: 'row',
+									alignItems: 'center',
+								}}
+							>
+								<View style={{ gap: 23 }} bgColor="transparent">
+									<TouchableOpacity style={styles.card}></TouchableOpacity>
+									<TouchableOpacity style={styles.card}></TouchableOpacity>
+								</View>
+								<View style={{ gap: 23 }} bgColor="transparent">
+									<TouchableOpacity style={styles.card}></TouchableOpacity>
+									<TouchableOpacity style={styles.card}></TouchableOpacity>
+								</View>
+								<View style={{ gap: 23 }} bgColor="transparent">
+									<TouchableOpacity style={styles.card}></TouchableOpacity>
+									<TouchableOpacity style={styles.card}></TouchableOpacity>
+								</View>
+							</ScrollView>
+						</View>
+					</View>
+				</View>
 			</View>
 		</>
 	)
@@ -36,32 +146,54 @@ const styles = StyleSheet.create({
 	},
 	containerChild: {
 		width: '100%',
-		borderWidth: 2,
-		borderColor: 'red',
+
 		flexDirection: 'row',
 		gap: 23,
 	},
 	containerChildWithMB: {
-		marginBottom: 40,
+		marginBottom: 20,
 	},
 	child: {
 		flex: 1,
-		borderWidth: 2,
-		borderColor: 'blue',
 	},
 	child2: {
 		flex: 1.4,
-		borderWidth: 2,
-		borderColor: 'blue',
 	},
 
-	title: {
-		fontSize: 20,
-		fontWeight: 'bold',
+	textTitle: {
+		fontFamily: fonts.fontFamyle.Gilroy_extraBold,
+		fontSize: fonts.size.lgSm,
+		color: Colors.light.sunsetOrange,
+		marginBottom: 8,
 	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: '80%',
+
+	mainCards: {
+		padding: 10,
+		borderRadius: 9,
+		flexDirection: 'row',
+		alignItems: 'center',
+		flexWrap: 'wrap',
+		gap: 25,
+	},
+	card: {
+		width: 217,
+		height: 123,
+		backgroundColor: Colors.light.sunsetOrange,
+		borderRadius: 9,
+	},
+	cardMeteorlogia: {
+		backgroundColor: Colors.light.periwinkleGray,
+		height: 280,
+		borderRadius: 9,
+	},
+	mainCardWhiteSmokeContainer: {
+		flexDirection: 'row',
+		gap: 6,
+	},
+	cardWhitesmoke: {
+		width: 94,
+		height: 70,
+		backgroundColor: Colors.light.smokeWhite,
+		borderRadius: 12,
 	},
 })
