b8768b117ec1ea3a059dc2a3b2d1657f107852f7
voideros <yalecomentador1@gmail.com>
2024-01-10 13:28:19 +0200

    UI banda desenhada

--- src/app/(entertainments)/comics/index.tsx
+++ src/app/(entertainments)/comics/index.tsx
@@ -1,10 +1,90 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import { HeaderModular } from '@/components/ui/HeaderModular'
+import { CardModular } from '@/components/ui/CardModular'
+import Colors from '@/constants/Colors'
+import { fonts } from '@/constants/fonts'
+import { ArraySectios } from '@/@types/interfaces'
+import { Href, Route } from 'expo-router'
 
 export default function Comics() {
+	const sectionsInfo: ArraySectios = [
+		{
+			href: `/(informations)/newspapers/${8}/${1}` as Route<string>,
+			cardTitles: ['Os Informais'],
+			img: {
+				imgType: 'img',
+				x: -140,
+				y: 100,
+				fit: 'contain',
+				height: 200,
+				width: 400,
+				image_url_import: require('@/assets/Thumbnails/assets_470.png'),
+			},
+		},
+		{
+			href: `/(informations)/newspapers/${8}/${1}` as Route<string>,
+			cardTitles: ['Banga'],
+			img: {
+				imgType: 'img',
+				x: -140,
+				y: 100,
+				fit: 'contain',
+				height: 200,
+				width: 400,
+				image_url_import: require('@/assets/Thumbnails/assets_472.png'),
+			},
+		},
+		{
+			href: `/(informations)/newspapers/${8}/${1}` as Route<string>,
+			cardTitles: ['Electus'],
+			img: {
+				imgType: 'img',
+				x: -140,
+				y: 100,
+				fit: 'contain',
+				height: 200,
+				width: 400,
+				image_url_import: require('@/assets/Thumbnails/assets_474.png'),
+			},
+		},
+	]
+
+	const width = 220
+	const height = 257
+
 	return (
-		<View>
-			<Text>Banda desenhada</Text>
-		</View>
+		<>
+			<HeaderModular isDefault={false} title="Banda Desenhada" />
+			<ScrollView
+				contentContainerStyle={{
+					paddingHorizontal: 50,
+					paddingVertical: 42,
+				}}
+			>
+				<View
+					style={{
+						flexDirection: 'row',
+						flexWrap: 'wrap',
+						gap: 20,
+					}}
+				>
+					{sectionsInfo.map((item, index) => {
+						return (
+							<View key={index}>
+								<CardModular
+									color={Colors.light.lavenderBlush}
+									font_size={fonts.size.md}
+									height={height}
+									width={width}
+									href={item.href}
+									cardTitles={item.cardTitles}
+									img={item.img}
+								/>
+							</View>
+						)
+					})}
+				</View>
+			</ScrollView>
+		</>
 	)
 }
