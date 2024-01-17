6ee1f30c092efcd498e3789f22d2611267487483
Antonio Sitoe <antoniositoehl@gmail.com>
2024-01-17 14:15:43 +0200

    interacao de media audio

---
+++ src/app/(entertainments)/novels/radionovels/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RadioNovelasList = () => {
+	return (
+		<View>
+			<Text>index</Text>
+		</View>
+	)
+}
+
+export default RadioNovelasList
+
+const styles = StyleSheet.create({})
--- src/services/trackPlayerServices.js
+++ src/services/trackPlayerServices.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-unsafe-finally */
 import TrackPlayer, {
 	AppKilledPlaybackBehavior,
 	Capability,
 	RepeatMode,
-	Event,
 } from 'react-native-track-player'
 
 export async function setupPlayer() {
@@ -38,44 +38,15 @@ export async function setupPlayer() {
 	}
 }
 
-export async function addTracks() {
-	await TrackPlayer.add([
-		{
-			id: '1',
-			url: require('@/assets/Audio/Hello.mp3'),
-			title: 'Hello',
-			artist: 'VOID',
-			artwork: require('@/assets/Thumbnails/assets_163.png'),
-			duration: 60,
-		},
-		{
-			id: '2',
-			url: require('@/assets/Audio/Lion.mp3'),
-			title: 'Lion',
-			artist: 'Florest',
-			artwork: require('@/assets/Thumbnails/assets_164.png'),
-			duration: 66,
-		},
-		{
-			id: '3',
-			url: require('@/assets/Audio/Birds.mp3'),
-			title: 'Birds',
-			artist: 'Beach',
-			artwork: require('@/assets/Thumbnails/assets_165.png'),
-			duration: 73,
-		},
-		{
-			id: '4',
-			url: require('@/assets/Audio/Dog.mp3'),
-			title: 'Dog',
-			artist: 'Home',
-			artwork: require('@/assets/Thumbnails/assets_448.png'),
-			duration: 73,
-		},
-	])
+export async function addTracks(playList) {
+	await TrackPlayer.add(playList)
 	await TrackPlayer.setRepeatMode(RepeatMode.Off)
 }
 
 export async function playbackService() {
-	// TODO: Attach remote event handlers
+	try {
+		// TODO: Attach remote event handlers
+	} catch (error) {
+		console.log('dsd')
+	}
 }
