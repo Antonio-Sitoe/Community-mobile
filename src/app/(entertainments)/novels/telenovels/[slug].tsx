93c5021b58b238d5906f32da366f18ca25d049fa
Mandlate <edson.mandlate@ubi.co.mz>
2024-01-19 11:13:11 +0200

    All Novels UI Done

--- src/app/(entertainments)/novels/telenovels/[slug].tsx
+++ src/app/(entertainments)/novels/telenovels/[slug].tsx
@@ -3,12 +3,6 @@ import MediaPlayer, { PlaylistItem } from '@/components/MediaPlayer'
 export default function NewsPappersEdition() {
 	const PLAYLIST = [
 		new PlaylistItem(
-			'Comfort Fit - “Sorry”',
-			require('@/assets/Audio/Lion.mp3'),
-			false,
-			require('@/assets/Thumbnails/assets_164.png'),
-		),
-		new PlaylistItem(
 			'Big Buck Bunny',
 			require('@/assets/Audio/big_buck_bunny.mp4'),
 			true,
@@ -17,12 +11,6 @@ export default function NewsPappersEdition() {
 			"Popeye - I don't scare",
 			'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4',
 			true,
-		),
-		new PlaylistItem(
-			'Podington Bear - “Rubber Robot”',
-			'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3',
-			false,
-			require('@/assets/Thumbnails/assets_164.png'),
 		),
 	]
 	return <MediaPlayer PLAYLIST={PLAYLIST} />
--- src/app/(entertainments)/videos/index.tsx
+++ src/app/(entertainments)/videos/index.tsx
@@ -1,10 +1,105 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { HeaderModular } from '@/components/ui/HeaderModular'
+import { svg1224, svg1225, svg1226, svg1227 } from '@/assets/Icons/out'
+import { ArraySectios } from '@/@types/interfaces'
+import { Route } from 'expo-router'
+import { CardModular } from '@/components/ui/CardModular'
+import { fonts } from '@/constants/fonts'
+import Colors from '@/constants/Colors'
 
 export default function Videos() {
+	const sectionsInfoRadio: ArraySectios = [
+		{
+			href: `/(entertainments)/videos/3` as Route<string>,
+			cardTitles: ['Nutrição'],
+			img: {
+				imgType: 'svg',
+				x: -10,
+				y: 70,
+				fit: 'contain',
+				height: 130,
+				width: 400,
+				image_url_import: svg1224,
+			},
+		},
+		{
+			href: `/(entertainments)/videos/3` as Route<string>,
+			cardTitles: ['Saúde'],
+			img: {
+				imgType: 'svg',
+				x: -10,
+				y: 70,
+				fit: 'contain',
+				height: 130,
+				width: 400,
+				image_url_import: svg1225,
+			},
+		},
+		{
+			href: `/(entertainments)/videos/3` as Route<string>,
+			cardTitles: ['Agricultura'],
+			img: {
+				imgType: 'svg',
+				x: -6,
+				y: 65,
+				fit: 'contain',
+				height: 130,
+				width: 400,
+				image_url_import: svg1226,
+			},
+		},
+		{
+			href: `/(entertainments)/videos/3` as Route<string>,
+			cardTitles: ['Higiene'],
+			img: {
+				imgType: 'svg',
+				x: -10,
+				y: 70,
+				fit: 'contain',
+				height: 130,
+				width: 400,
+				image_url_import: svg1227,
+			},
+		},
+	]
+
+	const width = 220
+	const height = 128.5
+
 	return (
-		<View>
-			<Text>Videos</Text>
-		</View>
+		<>
+			<HeaderModular isDefault={false} title="Vídeos" />
+			<ScrollView
+				contentContainerStyle={{
+					paddingHorizontal: 50,
+					paddingVertical: 42,
+				}}
+			>
+				<View
+					style={{
+						flexDirection: 'row',
+						flexWrap: 'wrap',
+						gap: 20,
+					}}
+				>
+					{sectionsInfoRadio.map((item, index) => {
+						return (
+							<View key={index}>
+								<CardModular
+									color={Colors.light.lavenderBlush}
+									font_size={fonts.size.md}
+									height={height}
+									width={width}
+									href={item.href}
+									cardTitles={item.cardTitles}
+									img={item.img}
+								/>
+							</View>
+						)
+					})}
+				</View>
+			</ScrollView>
+		</>
 	)
 }
