8e4aeb7cb240d600e8c7d82799b2ec9bc99c11b2
Antonio Sitoe <antoniositoehl@gmail.com>
2024-02-01 01:26:48 +0200

    geracao de dados MOKUPS

---
+++ src/utils/faker/generate_series.ts
@@ -0,0 +1,42 @@
+export const series_generate = {
+	data: [
+		{
+			id: 1,
+			name: 'Ouro Negro',
+			icon: require('@/assets/Thumbnails/assets_461.png'),
+			category_type: 'Radionovelas',
+		},
+
+		{
+			id: 3,
+			name: 'À Porta Da Minha Casa',
+			icon: require('@/assets/Thumbnails/assets_467.png'),
+			category_type: 'Radionovelas',
+		},
+		{
+			id: 10,
+			name: 'Janete',
+			icon: require('@/assets/Thumbnails/assets_469.png'),
+			category_type: 'Radionovelas',
+		},
+
+		{
+			id: 4,
+			name: 'Maida',
+			icon: require('@/assets/Thumbnails/assets_467.png'),
+			category_type: 'Telenovelas',
+		},
+		{
+			id: 5,
+			name: 'Kuga Munu',
+			icon: require('@/assets/Thumbnails/assets_474.png'),
+			category_type: 'Telenovelas',
+		},
+		{
+			id: 6,
+			name: 'Aquele Papo',
+			icon: require('@/assets/Thumbnails/assets_464.png'),
+			category_type: 'Telenovelas',
+		},
+	],
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,27 @@ export function capitalizeString(inputString: string) {
 		return inputString.charAt(0).toUpperCase() + inputString.slice(1)
 	else return ''
 }
+
+export function sliceStringsChars(str: string) {
+	if (str.length <= 35) {
+		return str
+	} else {
+		return str.substring(0, 32) + '...'
+	}
+}
+
+export function split_and_concat_string(str: string) {
+	if (str.length <= 20) {
+		return [str]
+	} else {
+		// Encontrar o índice do último espaço em branco antes do caractere 20
+		const lastIndex = str.lastIndexOf(' ', 20)
+
+		// Cortar a string na metade usando o índice encontrado
+		const primeiraParte = str.substring(0, lastIndex)
+		const segundaParte = str.substring(lastIndex + 1)
+
+		// Retornar as duas partes separadas por um espaço
+		return [primeiraParte, segundaParte]
+	}
+}
